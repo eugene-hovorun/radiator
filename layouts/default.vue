c1acff5272cdaff7f8c0bae509dea4ae4648a44d
eugene-hovorun <oktbvo@gmail.com>
2024-01-22 14:33:02 +0100

    feat: loader

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <slot />
+
+  <radio-loading v-if="showHideSpinner" />
+</template>
+
+<script lang="ts" setup>
+const showHideSpinner = ref(true);
+
+onMounted(() => {
+  showHideSpinner.value = false;
+});
+</script>
