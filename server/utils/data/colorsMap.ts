e3f4dc24fbe534d00894b821acefdc0131e71579
eugene-hovorun <oktbvo@gmail.com>
2024-01-28 19:47:45 +0100

    chore: revert colors

--- server/utils/data/colorsMap.ts
+++ server/utils/data/colorsMap.ts
@@ -1889,7 +1889,7 @@ export const colorsMap: {
     title: "Ukraine",
     slug: "ukraine",
     iso: "ua",
-    colors: ["gold", "#0057b8"],
+    colors: ["#0057b8", "gold"],
   },
   "united-arab-emirates": {
     title: "United Arab Emirates",
