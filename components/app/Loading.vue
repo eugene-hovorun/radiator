678abf8d188cf742a378d7903b29d6725ef9ad73
eugene-hovorun <oktbvo@gmail.com>
2024-03-02 16:59:21 +0100

    feat: loader animation

--- components/app/Loading.vue
+++ components/app/Loading.vue
@@ -14,6 +14,7 @@ div {
   display: grid;
   place-content: center;
   pointer-events: none;
+  animation: enlarge 1s infinite;
 }
 
 img {
@@ -21,5 +22,17 @@ img {
   height: 120px;
   pointer-events: none;
   opacity: 0.8;
+}
+
+@keyframes enlarge {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(0.8);
+  }
 }
 </style>
