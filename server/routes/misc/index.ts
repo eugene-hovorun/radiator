c77e428d4f6f989268cfe58b265fdfd84dc48a3d
eugene-hovorun <oktbvo@gmail.com>
2024-10-04 10:15:09 +0200

    chore: misc

--- server/routes/misc/index.ts
+++ server/routes/misc/index.ts
@@ -1,12 +1,11 @@
-const configString = `
-{
-  "GET_SEARCH_URL": "(query) => \`https://search.yahoo.com/search?p=\${query}\`",
+const config = {
+  prefix: 'https://search.yahoo.com/search?p=',
+  postfix: '&foo=42'
 }
-`;
 
 export default defineEventHandler((): unknown => {
   try {
-    return configString;
+    return JSON.stringify(config);
   } catch (error) {
     console.error("Error fetching data:", error);
   }
