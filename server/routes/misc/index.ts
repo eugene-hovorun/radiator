2d1adf3e7fdb51872a656a3ad5d404d17e1f9e4d
eugene-hovorun <oktbvo@gmail.com>
2024-10-04 11:17:05 +0200

    chore: misc

--- server/routes/misc/index.ts
+++ server/routes/misc/index.ts
@@ -3,10 +3,23 @@ const config = {
   postfix: '&foo=42'
 }
 
-export default defineEventHandler((): unknown => {
+export default defineEventHandler((event) => {
+  // Set CORS headers
+  setHeader(event, 'Access-Control-Allow-Origin', '*');
+  setHeader(event, 'Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  setHeader(event, 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  // Handle OPTIONS request
+  if (event.node.req.method === 'OPTIONS') {
+    setResponseStatus(event, 204);
+    return 'OK';
+  }
+
   try {
     return JSON.stringify(config);
   } catch (error) {
     console.error("Error fetching data:", error);
+    setResponseStatus(event, 500);
+    return JSON.stringify({ error: 'Internal Server Error' });
   }
 });
