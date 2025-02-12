65f9e7c7a156775119dbe406c58acbc482346707
eugene-hovorun <oktbvo@gmail.com>
2025-02-12 16:05:05 +0100

    fix: countries for radiator

--- server/routes/countries/index.ts
+++ server/routes/countries/index.ts
@@ -1,7 +1,17 @@
 import { api } from "../../utils";
 
-export default defineEventHandler(async (): Promise<Place[] | unknown> => {
+export default defineEventHandler(async (event): Promise<Place[] | unknown> => {
   try {
+    if (getHeader(event, "from_radiator")) {
+      setHeader(event, "Access-Control-Allow-Origin", "*");
+      setHeader(event, "Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+      setHeader(
+        event,
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+    }
+
     return await api.getCountries();
   } catch (error) {
     console.error("Error fetching data:", error);
